e2e2c5d32bb8fef25756d3856beae872ac869b52
missymaloney <missymaloney1@gmail.com>
2025-09-22 18:20:57 -0400

    first commit

---
+++ Models/CitiesRepository.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ServerManagementApp1.Models;
+
+public class CitiesRepository
+{
+  private static List<string> cities = new List<string>()
+  {
+    "Toronto",
+    "Montreal",
+    "Ottawa",
+    "Calgary",
+    "Halifax"
+  };
+
+  public static List<string> GetCities() => cities;
+
+}
